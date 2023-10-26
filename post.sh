9fc4c1791c1830c4d8945d9d86484e5165a7cc8e
zmatt <zmatt@zmatt9ML85.vmware.com>
2023-10-26 12:49:24 +0800

    20231026

--- post.sh
+++ post.sh
@@ -1,3 +1,3 @@
-git add ~/Documents/git/halfcoffee 
+git add ~/Documents/git/halfcoffee-archive
 git commit -m $1
-git push git@github.com:zm1990s/halfcoffee.git master
+git push git@github.com:zm1990s/halfcoffee-archive.git master
